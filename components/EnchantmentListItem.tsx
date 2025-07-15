cbec909bdba64377e889a698366d9a8b2b2a783b
Zenba <robson.rotmg@gmail.com>
2025-07-15 13:39:25 -0400

    white text on hover effect now

--- components/EnchantmentListItem.tsx
+++ components/EnchantmentListItem.tsx
@@ -17,7 +17,7 @@ const EnchantmentListItem: React.FC<EnchantmentListItemProps> = ({ enchantment, 
   return (
     <li className="group flex items-center justify-between py-3 px-5 border-b border-[#2c2f33] last:border-0 text-sm transition-all duration-75 cursor-pointer rounded-md hover:bg-slate-700/50 hover:shadow-xl hover:scale-[1.03] hover:-translate-y-1">
       <span
-        className="flex-grow text-[#b3b3b3] font-medium"
+        className="flex-grow text-[#b3b3b3] font-medium group-hover:text-white"
         dangerouslySetInnerHTML={{ __html: highlightedHtml }}
       />
       <div className="ml-4 flex-shrink-0">
